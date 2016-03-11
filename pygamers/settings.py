2b3ab0b774c4e7e9b7d0050e66a5d1cb7a51d6fa
teldridge11 <tom.eldridge@hotmail.com>
2016-03-11 18:02:44 -0500

    static moved to pygamers app

--- pygamers/settings.py
+++ pygamers/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['www.pygamers.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'pygamers',
     'userprofile',
     'accounts',
     'games',
@@ -77,22 +78,23 @@ WSGI_APPLICATION = 'pygamers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd6qb0pni46nonn',
-        'USER': 'paedswhqtydxnt',
-        'PASSWORD': 'f7040uVvzDmEtO5yKR6ozhvc55',
-        'HOST': 'ec2-54-83-56-31.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
-
-#'default': {
-#    'ENGINE': 'django.db.backends.sqlite3',
-#    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'd6qb0pni46nonn',
+#        'USER': 'paedswhqtydxnt',
+#        'PASSWORD': 'f7040uVvzDmEtO5yKR6ozhvc55',
+#        'HOST': 'ec2-54-83-56-31.compute-1.amazonaws.com',
+#        'PORT': '5432',
+#    }
 #}
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
